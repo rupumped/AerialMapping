b60a8535c82aaef99d82939b5b3407ece2e4cee7
rupumped <rupumped@gmail.com>
2018-08-23 11:13:25 -0400

    Removed deprecated files

--- Path_Planning/Deprecated/sortPaths.m
+++
@@ -1,6 +0,0 @@
-function [paths, scores] = sortPaths(paths)
-nodes = {paths.Nodes};
-scores = cellfun(@(p) numel(unique(p)), nodes);
-[scores,ind] = sort(scores,'descend');
-paths = paths(ind);
-end
--- Path_Planning/Deprecated/visualizePaths.m
+++
@@ -1,9 +0,0 @@
-function visualizePaths(g,paths,Hz,holding)
-close all
-for p=paths
-    h = plot(g,'b');
-    highlight(h,p.Nodes,'NodeColor','g','EdgeColor','m','LineWidth',3,'MarkerSize',5)
-    if holding, hold on; end
-    pause(1/Hz)
-end
-end
